8ca7dbf2615a934bc273ba389746d3959f38e5ba
Betelhem357 <mbetelhem2017@gmail.com>
2021-07-07 21:18:34 -0500

    make start on click

--- w3d3/maze.js
+++ w3d3/maze.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     //$("#boundary1").mouseover(turnRed).mouseleave(removeRed);
     let pass_through_flag = false; 
-    $("div.boundary").mouseover(turnRed);
+    let start = false;
+    $("div.boundary").mouseover(turnRed);//turn red when the user hover over 
     $("div#end").mouseover(winMessage);
-    $("div#start").mouseover(reset);
-    $("#maze").mouseleave (handlemouseout);
+    $("div#start").click(reset).mouseleave();
+    // $("#maze").mouseleave (handlemouseout);
     function turnRed(){
         $("div.boundary").addClass("youlose");
         pass_through_flag=true;
@@ -26,9 +27,11 @@ $(document).ready(function(){
         $("#status").text("Click the \"S\" to begin. ");
         
     }
-    function handlemouseout(){
-        $("div.boundary").addClass("youlose");
-        $("#status").text("Sorry, you lost. :[");
-        pass_through_flag=true;
-    }
+    // function handlemouseout(){
+    //     $("#maze").mouseleave (function(){
+    //         $("div.boundary").addClass("youlose");
+    //         $("#status").text("Sorry, you lost. :[");
+    //         pass_through_flag=true;
+    //     });
+    // }
 });
