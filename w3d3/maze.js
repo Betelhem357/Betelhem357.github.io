00e532071c00c9dcc1174bf38e8c974f4c6679f6
Betelhem357 <mbetelhem2017@gmail.com>
2021-07-07 22:07:42 -0500

    avoid cheating on game

--- w3d3/maze.js
+++ w3d3/maze.js
@@ -1,17 +1,18 @@
 $(document).ready(function(){
     //$("#boundary1").mouseover(turnRed).mouseleave(removeRed);
-    let pass_through_flag = false; 
+    let win = true; 
     let start = false;
     $("div.boundary").mouseover(turnRed);//turn red when the user hover over 
     $("div#end").mouseover(winMessage);
-    $("div#start").click(reset).mouseleave();
-    // $("#maze").mouseleave (handlemouseout);
+    $("div#start").click(reset);
+    $("#maze").mouseleave (handlemouseout);
     function turnRed(){
         $("div.boundary").addClass("youlose");
-        pass_through_flag=true;
+        win = false;
     }
     function winMessage(){
-        if(!pass_through_flag){
+        if(start){
+            if(win){
                 //alert("You win!");
                 $("#status").text("You win! :]");
             }
@@ -19,19 +20,22 @@ $(document).ready(function(){
                // alert("Sorry, you lost. :[");
                $("#status").text("Sorry, you lost. :[");
             }
+            start = false;
+        }
         
     }
     function reset(){
-        pass_through_flag=false;
+        win = true;
         $("div.boundary").removeClass("youlose");
         $("#status").text("Click the \"S\" to begin. ");
+        start = true;
         
     }
-    // function handlemouseout(){
-    //     $("#maze").mouseleave (function(){
-    //         $("div.boundary").addClass("youlose");
-    //         $("#status").text("Sorry, you lost. :[");
-    //         pass_through_flag=true;
-    //     });
-    // }
+    function handlemouseout(){
+            if(start){
+                $("div.boundary").addClass("youlose");
+                $("#status").text("Sorry, you lost. :[");
+                win = false;
+            }
+    }
 });
