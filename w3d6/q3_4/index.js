320da27d471de20ccb7581de7a202627fb469ff1
Betelhem357 <mbetelhem2017@gmail.com>
2021-07-12 00:41:44 -0500

    modification on 3 and 4

--- w3d6/q3_4/index.js
+++ w3d6/q3_4/index.js
@@ -1,26 +1,34 @@
 let express = require('express');
+let path = require('path');
 let app = express();
 app.set('view engine','ejs');
+app.use('/css', express.static(path.join( __dirname,'css')));
 let products = [{
-   name:'betty',
+   name:'product',
    price:50,
    quantity:12
 },{
-    name:'betty2',
+    name:'product2',
     price:70,
     quantity:50
 }
 ];
-let name = "yordi";
+let name = "product1";
 let price = 70;
 let description = "This product is awsome product :)";
 let id = "id123";
+
 app.get('/',(req,res)=>{
-   res.render('shoppingcart.ejs',{products});
+   res.render('product.ejs',{name:name,price:price,id:id,description:description});
 });
 
 app.get('/cart',(req,res)=>{
-   res.render('product.ejs',{name:name,price:price,id:id,description:description});
+   res.render('shoppingcart.ejs',{products});
 });
+
+app.post('/addToCart', (req, res, next) => {
+   res.redirect('/cart');
+});
+
 
 app.listen(3000);
